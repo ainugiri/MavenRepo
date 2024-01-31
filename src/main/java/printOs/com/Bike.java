c5324fa5c160240774ec63cbc06277af1013363e
ainugiri <40005294+ainugiri@users.noreply.github.com>
2024-01-31 11:26:11 +0530

    Update Bike.java

--- src/main/java/printOs/com/Bike.java
+++ src/main/java/printOs/com/Bike.java
@@ -3,5 +3,8 @@ class Bike{
   public static void main(String args[])
   {
     System.out.println("Welcome to Bike class show room");
+    System.out.println("Welcome to Bike class show room");
+    System.out.println("Welcome to Bike class show room");
+    System.out.println("Welcome to Bike class show room");
   }
 }
