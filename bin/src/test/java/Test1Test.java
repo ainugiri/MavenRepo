4bcbae4e1c940adb30a2b11d905ef8466c05cdf5
ainugiri <giriprasa@gmail.com>
2024-04-24 09:59:16 +0530

    "Test Added"

---
+++ bin/src/test/java/Test1Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Test1Test {
+
+    @Test
+    public void testConstructor() {
+        Test1 test1 = new Test1();
+        assertNotNull(test1);
+    }
+}
